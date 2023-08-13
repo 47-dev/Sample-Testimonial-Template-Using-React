3e781d7b41d91859908e680d7a353c1ba43da49b
RAJVEER SINGH <79639577+47-dev@users.noreply.github.com>
2023-08-13 23:10:19 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,26 @@
+const reviews = [
+  {
+    id: 1,
+    name: "Rajveer Singh",
+    job: "Software Engineer",
+    image: "https://avatars.githubusercontent.com/u/79639577?v=4",
+    text: "I have had the pleasure of working with this team on several projects, and I am consistently impressed with their technical expertise and ability to deliver quality solutions on time and within budget. They are a true partner and an asset to any project.",
+  },
+  {
+    id: 2,
+    name: "Ayush Kumar",
+    job: "Competitive Programmer",
+    image: "https://avatars.githubusercontent.com/u/91387353?v=4",
+    text: "I had the opportunity to work with this team on a data analysis project, and I was impressed with their attention to detail and their ability to work with complex datasets. They provided valuable insights that helped us make informed business decisions. It was a pleasure working with them.",
+  },
+  {
+    id: 3,
+    name: "Swapnil Sachan",
+    job: "FULL STACK DEVELOPER",
+    image: "https://avatars.githubusercontent.com/u/63530845?v=4",
+    text: "I have been working with this company for several years now, and I have always been impressed with their creativity and attention to detail. They are true professionals who take pride in their work and always go above and beyond to deliver exceptional results.",
+  },
+  
+];
+
+export default reviews;
